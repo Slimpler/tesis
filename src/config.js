681a4880848f094fb629d2cf94adc4ecd1a64985
Slimpler <nicolas.delgado@alumnos.uv.cl>
2023-08-14 07:46:54 -0400

    probando

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const PORT = process.env.PORT || 4001;
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:10000/tesis-nico";
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/tesis-nico";
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
-export const FRONTEND_URL = process.env.FRONTEND_URL || "https://lucent-valkyrie-cf7d4f.netlify.app/";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
