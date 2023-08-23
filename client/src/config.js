6ed9dada6148cd07fd26567345a9bfd484ab3c32
Slimpler <nicolas.delgado@alumnos.uv.cl>
2023-08-23 11:26:57 -0400

    probando

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 
-export const API_URL = import.meta.env.VITE_API_URL|| "http://localhost:4001/api";
+export const API_URL = import.meta.env.VITE_API_URL|| "https://lucent-valkyrie-cf7d4f.netlify.app/api";
   
