1d8525797d331218006ec3a989dae5db32be28c6
Slimpler <nicolas.delgado@alumnos.uv.cl>
2023-08-29 19:12:58 -0400

    volviendo

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,5 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./context/authContext";
+import { useEffect, useState } from "react";
+import axios from "./api/axios";
 
 export const ProtectedRoute = () => {
   const { isAuthenticated, loading } = useAuth();
@@ -18,7 +20,7 @@ export const ProtectedRouteAdmin = () => {
   // console.log('admin')
 
   if (loading) return <h1>Loading...</h1>;
-  if (!isAuthenticated && !loading) return <Navigate to="/" replace />; 
+  if (!isAuthenticated && !loading) return <Navigate to="/" replace />;
 
   if (user.roles.includes("admin")) {
     return <Outlet />;
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173, // Puerto para el servidor de desarrollo
-  },
-  build: {
-    outDir: 'dist', // Directorio de salida para la versión de producción
-    minify: true, // Habilitar la minificación en producción
-    manifest: true, // Agregar hash a los nombres de archivo para el control de caché
-  },
+  plugins: [react()]
 })
