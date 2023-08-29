d797807ce67f04f06b48392ba3e9a00cf5530263
Slimpler <nicolas.delgado@alumnos.uv.cl>
2023-08-29 16:52:09 -0400

    same

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -1,7 +1,5 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "./context/authContext";
-import { useEffect, useState } from "react";
-import axios from "./api/axios";
 
 export const ProtectedRoute = () => {
   const { isAuthenticated, loading } = useAuth();
@@ -20,7 +18,7 @@ export const ProtectedRouteAdmin = () => {
   // console.log('admin')
 
   if (loading) return <h1>Loading...</h1>;
-  if (!isAuthenticated && !loading) return <Navigate to="/" replace />;
+  if (!isAuthenticated && !loading) return <Navigate to="/" replace />; 
 
   if (user.roles.includes("admin")) {
     return <Outlet />;
