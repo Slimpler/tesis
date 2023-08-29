7c2fb9a067be9ff892944ef5ad1a2b2e7e363138
Slimpler <nicolas.delgado@alumnos.uv.cl>
2023-08-29 18:19:50 -0400

    nose

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: 5173, // Puerto para el servidor de desarrollo
+  },
+  build: {
+    outDir: 'dist', // Directorio de salida para la versión de producción
+    minify: true, // Habilitar la minificación en producción
+    manifest: true, // Agregar hash a los nombres de archivo para el control de caché
+  },
 })
