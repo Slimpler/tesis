024584eee92a9bfb5abe07e14a2efc3aa213e65a
Slimpler <nicolas.delgado@alumnos.uv.cl>
2023-09-04 01:12:12 -0300

    probando

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -31,7 +31,7 @@ export const login = async (req, res) => {
     });
 
     res.cookie("token", token, {
-      httpOnly: false,
+      // httpOnly: false,
       // signed: true,
       // secure: true,
       sameSite: "none",
