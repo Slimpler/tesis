2ddeb19037ef92cef3e83f008217e949f25cf7db
Slimpler <nicolas.delgado@alumnos.uv.cl>
2023-09-04 00:51:37 -0300

    probando

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -5,6 +5,7 @@ import User from "../models/user.model.js";
 export const auth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
+    console.log(req.cookies);
 
     if (!token) {
       return res.status(401).json({ message: "No token, authorization denied" });
