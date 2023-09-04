000725bd1236ed083bfe76122f0202e134002d8e
Slimpler <nicolas.delgado@alumnos.uv.cl>
2023-09-04 00:51:31 -0300

    probando

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -3,24 +3,20 @@ import axios from "./axios";
 export const registerRequest = async (user) =>
   axios.post(`/auth/register`, user);
 
-export const loginRequest = async (user) => 
-  axios.post(`/auth/login`, user);
+export const loginRequest = async (user) => axios.post(`/auth/login`, user);
 
-  export const verifyTokenRequest = async () => {
-    try {
-      const response = await axios.get(`/auth/verify`, {
-        params: {
-          timestamp: new Date().getTime(), 
-        },
-        withCredentials: true,
-      });
-      return response;
-    } catch (error) {
-      throw error;
-    }
-  };
+export const verifyTokenRequest = async () => {
+  try {
+    const response = await axios.get(`/auth/verify`, {
+      params: {
+        timestamp: new Date().getTime(),
+      },
+      withCredentials: true,
+    });
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export const logoutRequest = async () =>
-  axios.post(`/auth/logout`, {
-    withCredentials: true,
-  });
+export const logoutRequest = async () => axios.post(`/auth/logout`);
